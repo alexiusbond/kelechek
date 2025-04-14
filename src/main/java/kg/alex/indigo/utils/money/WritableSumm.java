ed14a0e1824c4c00522f85bbec653d5c1f23785f
alexiusbond <aleksey.bondarev@iaau.edu.kg>
2025-04-14 16:23:05 +0600

    lomonosov first

--- src/main/java/kg/alex/indigo/utils/money/WritableSumm.java
+++
@@ -1,7 +0,0 @@
-package kg.alex.indigo.utils.money;
-
-public interface WritableSumm {
-    String numberToString(Number num);
-}
-
-
--- src/main/resources/backup.sh
+++ src/main/resources/backup.sh
@@ -1,7 +1,7 @@
 now="$(date +'%d_%m_%Y_%H_%M_%S')"
 filename="db_backup_$now".gz
-backupfolder="/home/indigo/backups"
+backupfolder="/home/lomonosov/backups"
 fullpathbackupfile="$backupfolder/$filename"
-mysqldump --user=root --password=AiU2017dB --default-character-set=utf8 indigo | gzip > "$fullpathbackupfile"
+mysqldump --user=root --password=AiU2017dB --default-character-set=utf8 lomonosov | gzip > "$fullpathbackupfile"
 chmod 777 "$fullpathbackupfile"
 exit 0
